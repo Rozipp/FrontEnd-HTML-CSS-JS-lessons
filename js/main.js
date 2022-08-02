d4b192a1068c64faa3c503125cb6216dd6455e0c
Rozipp <Royashch@gmail.com>
2022-08-02 11:46:11 +0300

    Закончил2

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ $(".navbar a").click(function() {
         scrollTop: $("#" + $(this).data('value')).offset().top
     }, 1000)
 })
+
+function begin() {
+    $("body,html").animate({
+        scrollTop: $("#about").offset().top
+    }, 1000)
+}
